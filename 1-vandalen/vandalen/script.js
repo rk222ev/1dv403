2ce4376ebb5a5a2ed19ca6e09702a7d13c59a1b3
Robin Karlsson <rk222ev@github.com>
2014-10-28 21:26:19 +0100

    Playing around with the code.

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,9 +1,28 @@
 "use strict";
 
 var makePerson = function(persArr){
+	var persons = persArr.concat();
+	var today = new Date();
+	var names = "";
+	var nameArr = [];
+
+	// Collectes and sorts the names
+	names = persons.forEach(function(person){
+		nameArr.push(person.name);
+	});
+
+	names = nameArr.sort().join(", ");
 
 
-	// Din kod här...
+	// Sorts on born.
+	// persons.sort(function (a, b) {
+	// 		if (a.born > b.born) {
+	// 			return 1;
+	// 		} else if (a.born < b.born) {
+	// 			return -1;
+	// 		}
+	// 		return 0;
+	// });
 
-}
-
+	return {names: names, minAge: -1, maxAge: -1, averageAge: -1};
+};
