901ea81e7903ad3b6719947ded36b2ff991b9920
Robin Karlsson <rk222ev@github.com>
2014-10-28 21:36:46 +0100

    Removed and rewritten the code.

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -3,26 +3,19 @@
 var makePerson = function(persArr){
 	var persons = persArr.concat();
 	var today = new Date();
-	var names = "";
-	var nameArr = [];
+	var names= [];
+	var maxAge = [];
 
 	// Collectes and sorts the names
-	names = persons.forEach(function(person){
-		nameArr.push(person.name);
+	persons.forEach(function(person){
+		names.push(person.name);
 	});
 
-	names = nameArr.sort().join(", ");
 
-
-	// Sorts on born.
-	// persons.sort(function (a, b) {
-	// 		if (a.born > b.born) {
-	// 			return 1;
-	// 		} else if (a.born < b.born) {
-	// 			return -1;
-	// 		}
-	// 		return 0;
-	// });
-
-	return {names: names, minAge: -1, maxAge: -1, averageAge: -1};
+	return {
+		names: names.sort().join(", "),
+		minAge: -1,
+		maxAge: -1,
+		averageAge: -1
+	};
 };
