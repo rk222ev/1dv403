f8c7eccacdf6ea3de3c2475475b090632bbc171a
Robin Karlsson <rk222ev@github.com>
2014-10-28 12:19:33 +0100

    Added basic logic to determine of a guess is lower, higher etc and return apropriate feedback.

--- 1-vandalen/guess/script.js
+++ 1-vandalen/guess/script.js
@@ -1,26 +1,40 @@
-"use strict";
-
+//"use strict";
 window.onload = function(){
-	
-	var secret = 50; // Detta tal behöver bytas ut mot ett slumpat tal.
-	
-	// I denna funktion ska du skriva koden för att hantera "spelet"
-	var guess = function(number){
-		console.log("Det hemliga talet: " + secret); // Du når den yttre variabeln secret innifrån funktionen.
-		console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
-			
-		// Plats för förändring.
+    var secret = 50; // Detta tal behöver bytas ut mot ett slumpat tal.
+    var guesses = 0;
+
+    // I denna funktion ska du skriva koden för att hantera "spelet"
+    var guess = function (number) {
+    	console.log("Det hemliga talet: " + secret); // Du når den yttre variabeln secret innifrån funktionen.
+    	console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
+
+        if (number < 1 || number > 100) {
+            return [false, "Talet är utanför intervallet 0 - 100"];
+        } else if (isNaN(number)) {
+            return [false, "Gissningen kunde inte tolkas som en siffra."];
+        }
+
+        guesses += 1;
+
+        if (+number === secret) {
+            return [true, "Grattis du vann! Det hemliga talet var "+ secret +" och du behövde "+guesses+" gissningar för att hitta det."];
+        } else if (number < 1 || number > 100) {
+            return [false, "Talet är utanför intervallet 0 - 100"];
+        } else if (number < secret) {
+            return [false, "Det hemliga talet är högre!"];
+        } else if (number > secret) {
+            return [false, "Det hemliga talet är lägre!"];
+        }
 
 
-		// Returnera exempelvis: 
-		// [true, "Grattis du vann! Det hemliga talet var X och du behövde Y gissningar för att hitta det."]
-		// [false, "Det hemliga talet är högre!"]
-		// [false, "Det hemliga talet är lägre!"]
-		// [false, "Talet är utanför intervallet 0 - 100"]		
-	};
-	
+    	// Returnera exempelvis:
+    	// [true, "Grattis du vann! Det hemliga talet var X och du behövde Y gissningar för att hitta det."]
+    	// [false, "Det hemliga talet är högre!"]
+    	// [false, "Det hemliga talet är lägre!"]
+    	// [false, "Talet är utanför intervallet 0 - 100"]
+    };
+
 	// ------------------------------------------------------------------------------
-
 
 
 	// Kod för att hantera utskrift och inmatning. Denna ska du inte behöva förändra
@@ -32,12 +46,12 @@ window.onload = function(){
 	submit.addEventListener("click", function(e){
 		e.preventDefault(); // Hindra formuläret från att skickas till servern. Vi hanterar allt på klienten.
 
-		var answer = guess(input.value) // Läser in talet från textrutan och skickar till funktionen "guess"
-		p.innerHTML = answer[1];		// Skriver ut texten från arrayen som skapats i funktionen.	
+		var answer = guess(input.value); // Läser in talet från textrutan och skickar till funktionen "guess"
+		p.innerHTML = answer[1];		// Skriver ut texten från arrayen som skapats i funktionen.
 
 		if(answer[0] === true){				// Om spelet är slut, avaktivera knappen.
 			submit.disabled = true;
 		}
-	
+
 	});
 };
