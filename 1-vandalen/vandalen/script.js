ac28ded865696348f60b62c992bef3fb501d993e
Robin Karlsson <rk222ev@github.com>
2014-10-29 09:32:12 +0100

    Rewritten alot of code. Looks completely different but does the same thing.

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,24 +1,22 @@
 "use strict";
 
 var makePerson = function(persArr){
-	var persons = persArr.concat();
-	var collator = new Intl.Collator(["se"]);
 	var names = [];
 	var ages = [];
-	var sum = 0;
 
-	persons.forEach(function(person){
+	persArr.forEach(function(person){
 		names.push(person.name);
 		ages.push(person.age);
-		sum += person.age;
 	});
 
 	ages.sort();
 
 	return {
-		names: names.sort(collator.compare).join(", "),
+		names: names.sort().join(", "),
 		minAge: ages[0],
-		maxAge: ages[ages.length -1],
-		averageAge: Math.round(sum/ages.length),
+		maxAge: ages[ages.length - 1],
+		averageAge: Math.round(ages.reduce(function(a, b) {
+			return a + b;
+		 }) / ages.length),
 	};
 };
