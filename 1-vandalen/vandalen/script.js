8360e3b060a6cd9872b3a9f1a90680f17cff6a4a
Robin Karlsson <rk222ev@github.com>
2014-10-29 13:13:32 +0100

    Added check on names and ages.

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -3,17 +3,27 @@
 var makePerson = function(persArr){
 	var names = [];
 	var ages = [];
-	var average = function(numbers) {
+
+	function average(numbers) {
 			return numbers.reduce(function(a, b) { return a + b;}) / numbers.length;
-	};
+	}
 
 	persArr.map(function(person){
-		names.push(person.name);
-		ages.push(person.age);
+		if (typeof person.name === "string") {
+			names.push(person.name);
+		} else {
+			names.push("John Doe");
+		}
+		if (typeof person.age === "number") {
+			ages.push(person.age);
+		} else {
+			ages.push(NaN);
+		}
+
 	});
 
 	ages.sort();
-
+	
 	return {
 		names: names.sort(function(a, b) { return a.localeCompare(b); }).join(", "),
 		minAge: ages[0],
