e3a9e02c26e4da574390eea866414d9d7b4f80de
Robin Karlsson <rk222ev@github.com>
2014-10-29 20:32:49 +0100

    Removed error handling.

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -9,21 +9,12 @@ var makePerson = function(persArr){
 	}
 
 	persArr.map(function(person){
-		if (typeof person.name === "string") {
-			names.push(person.name);
-		} else {
-			names.push("John Doe");
-		}
-		if (typeof person.age === "number") {
-			ages.push(person.age);
-		} else {
-			ages.push(NaN);
-		}
-
+		names.push(person.name);
+		ages.push(person.age);
 	});
 
 	ages.sort();
-	
+
 	return {
 		names: names.sort(function(a, b) { return a.localeCompare(b); }).join(", "),
 		minAge: ages[0],
