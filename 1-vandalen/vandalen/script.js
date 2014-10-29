119dd5d0b3be32f116cf19d98182a1f029b6c689
Robin Karlsson <rk222ev@github.com>
2014-10-29 09:54:37 +0100

    Done some cleanup on how averege is calculated and ÅÄÖ is now sorted correctly. Passes all tests.

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -3,6 +3,9 @@
 var makePerson = function(persArr){
 	var names = [];
 	var ages = [];
+	var average = function(numbers) {
+			return numbers.reduce(function(a, b) { return a + b;}) / numbers.length;
+	};
 
 	persArr.forEach(function(person){
 		names.push(person.name);
@@ -12,11 +15,9 @@ var makePerson = function(persArr){
 	ages.sort();
 
 	return {
-		names: names.sort().join(", "),
+		names: names.sort(function(a, b) { return a.localeCompare(b); }).join(", "),
 		minAge: ages[0],
 		maxAge: ages[ages.length - 1],
-		averageAge: Math.round(ages.reduce(function(a, b) {
-			return a + b;
-		 }) / ages.length),
+		averageAge: Math.round(average(ages))
 	};
 };
