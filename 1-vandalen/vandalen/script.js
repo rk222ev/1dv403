6d9a25131f271c556205262bfbace4b95ac68971
Robin Karlsson <rk222ev@github.com>
2014-10-31 13:35:47 +0100

    Played around testing different thing. Look pretty good at the moment I think.

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,37 +1,40 @@
 "use strict";
 
 var makePerson = function(persArr){
-
 		var ages = [];
-
-		function average(numbers) {
-			return numbers.reduce(function(a, b) { return a + b;}) / numbers.length;
-		}
-
-		function getKeyValues(key, arr) {
-
-			return arr.map(function(arr) {
-				 return arr[key];
-			});
-		}
+		var average;
+		var names = [];
 
 	try {
 
-		if (!Array.isArray(persArr) && !("name" in persArr) && !("age" in persArr)) {
-			throw new TypeError("Varible is not an array containing the name and age keys.");
-		}
 
-		ages = getKeyValues("age", persArr);
+		names = persArr.map(function(person) {return person.name;}).sort(function(a, b) { return a.localeCompare(b); }).join(", ");
 
+		ages = persArr.map(
+			function(person) {
+				if ("age" in person) {
+					return person.age;
+				} else if ("born" in person) {
+						return new Date().getFullYear() - new Date(person.born).getFullYear();
+				} else {
+					throw new Error("Cant find the member age or born.");
+				}
+			}
+		);
+
+
+	 	average = function(numbers) {
+			return numbers.reduce(function(a, b) { return a + b;}) / numbers.length;
+		};
 
 		return {
-			// Sorterar alla nycklar från perArr med lokalatecken och joinar med , mellan varje medlem.
-			names: 		getKeyValues("name", persArr).sort(function(a, b) { return a.localeCompare(b); }).join(", "),
+			names: 		names,
 			minAge: 	Math.min.apply(null, ages),
 			maxAge: 	Math.max.apply(null, ages),
 			averageAge: Math.round(average(ages))
 		};
 	} catch (error){
-		// Do something.
+		//console.log(error);
+		throw new Error(error);
 	}
 };
