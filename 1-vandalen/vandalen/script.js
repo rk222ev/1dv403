26ef57c4b006b9d4f6fae6bbdb9c164235fa27a1
Robin Karlsson <rk222ev@github.com>
2014-10-31 12:24:17 +0100

    Added Try catch and a thrown error.

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -2,26 +2,36 @@
 
 var makePerson = function(persArr){
 
-	var ages = getKeyValues("age", persArr);
-	
-	function average(numbers) {
-		return numbers.reduce(function(a, b) { return a + b;}) / numbers.length;
+		var ages = [];
+
+		function average(numbers) {
+			return numbers.reduce(function(a, b) { return a + b;}) / numbers.length;
+		}
+
+		function getKeyValues(key, arr) {
+
+			return arr.map(function(arr) {
+				 return arr[key];
+			});
+		}
+
+	try {
+
+		if (!Array.isArray(persArr) && !("name" in persArr) && !("age" in persArr)) {
+			throw new TypeError("Varible is not an array containing the name and age keys.");
+		}
+
+		ages = getKeyValues("age", persArr);
+
+
+		return {
+			// Sorterar alla nycklar från perArr med lokalatecken och joinar med , mellan varje medlem.
+			names: 		getKeyValues("name", persArr).sort(function(a, b) { return a.localeCompare(b); }).join(", "),
+			minAge: 	Math.min.apply(null, ages),
+			maxAge: 	Math.max.apply(null, ages),
+			averageAge: Math.round(average(ages))
+		};
+	} catch (error){
+		// Do something.
 	}
-
-	function getKeyValues(key, arr) {
-
-		return arr.map(function(arr) {
-			 return arr[key];
-		});
-
-	}
-
-
-	return {
-		// Sorterar alla nycklar från perArr med lokalatecken och joinar med , mellan varje medlem.
-		names: 		getKeyValues("name", persArr).sort(function(a, b) { return a.localeCompare(b); }).join(", "),
-		minAge: 	Math.min.apply(null, ages),
-		maxAge: 	Math.max.apply(null, ages),
-		averageAge: Math.round(average(ages))
-	};
 };
