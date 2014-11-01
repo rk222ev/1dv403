e9cba7688dcc715047668727d064e2e05f0d5dea
Robin Karlsson <rk222ev@github.com>
2014-11-01 19:03:47 +0100

    Worked on the HTML and CSS.

--- 2-labbymezzage/js/MessageBoard.js
+++ 2-labbymezzage/js/MessageBoard.js
@@ -1,12 +1,16 @@
 "use strict";
-window.onload = function() {
-    engine.init();
-};
 
-var engine = {
+var MessageBoard= {
+
+        messages: [],
+
         init: function () {
             var test = new Message("Testar massa", new Date());
 
             console.log(test);
         }
+};
+
+window.onload = function() {
+    MessageBoard.init();
 };
