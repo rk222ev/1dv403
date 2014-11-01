bc38dfb5f29e66ded1c275e83adadfd4b1d3b94e
Robin Karlsson <rk222ev@github.com>
2014-11-01 20:19:28 +0100

    Made messageboard to a function. Switched to objects. Added messages array. And pushed the textarea value to the mentioned array.

--- 2-labbymezzage/js/MessageBoard.js
+++ 2-labbymezzage/js/MessageBoard.js
@@ -1,16 +1,44 @@
 "use strict";
 
-var MessageBoard= {
+// var MessageBoard= {
+//
+//         messages: [],
+//
+//         init: function () {
+//             var test = new Message("Testar massa", new Date());
+//
+//             console.log(test);
+//         }
+// };
 
-        messages: [],
 
-        init: function () {
-            var test = new Message("Testar massa", new Date());
 
-            console.log(test);
-        }
-};
+function MessageBoard (name) {
+    var that = this;
+
+    var div = document.getElementById(name);
+
+    // Send button
+    var inputButton = document.createElement("input");
+    inputButton.type = "button";
+    inputButton.value = "skriv";
+    // inputButton.onclick = function (e) { that.sendMessage(); return false;};
+    inputButton.onclick = function (e) {
+        //var test = div.getElementByClassName("message");
+        // console.log(div.getElementsByClassName("message"));
+        that.sendMessage(div.getElementsByClassName("message")[0].value); return false;
+    };
+
+    div.appendChild(inputButton);
+
+    that.sendMessage = function (message) {
+        that.messages.push(message);
+    };
+
+    that.messages = [];
+}
 
 window.onload = function() {
-    MessageBoard.init();
+    var MessageBoard1 = new MessageBoard("board1");
+    var MessageBoard2 = new MessageBoard("board2");
 };
