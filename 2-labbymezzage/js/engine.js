39011cafe662127cdd5482eb2535a6a001a0f66a
Robin Karlsson <rk222ev@github.com>
2014-11-01 16:48:43 +0100

    Done some work on the message constructor.

--- 2-labbymezzage/js/engine.js
+++ 2-labbymezzage/js/engine.js
@@ -1,4 +1,9 @@
 "use strict";
 window.onload = function() {
 
+    var test = new Message("Testar massa", new Date());
+
+
+    console.log(test);
+
 };
--- 2-labbymezzage/js/message.js
+++ 2-labbymezzage/js/message.js
@@ -1,4 +1,24 @@
 "use strict";
-window.onload = function() {
-    console.log("test");
+
+function Message(message, date) {
+
+    this.getText = function () {
+        return message;
+    };
+
+    this.setText = function (text) {
+        message = text;
+    };
+
+    this.getDate = function () {
+        return date;
+    };
+
+    this.setDate = function (argDate) {
+        date = argDate;
+    };
+}
+
+Message.prototype.toString = function () {
+    return this.getText() + " ( " + this.getDate() + ")";
 };
