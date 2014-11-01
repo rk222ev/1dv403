b538293c60c622f0982789d008d72a377c0b36e2
Robin Karlsson <rk222ev@github.com>
2014-11-01 21:17:38 +0100

    Played around with appending messages to the message div.

--- 2-labbymezzage/js/MessageBoard.js
+++ 2-labbymezzage/js/MessageBoard.js
@@ -15,30 +15,61 @@
 
 function MessageBoard (name) {
     var that = this;
-
     var div = document.getElementById(name);
+
+    that.messages = [];
 
     // Send button
     var inputButton = document.createElement("input");
     inputButton.type = "button";
     inputButton.value = "skriv";
-    // inputButton.onclick = function (e) { that.sendMessage(); return false;};
+
     inputButton.onclick = function (e) {
-        //var test = div.getElementByClassName("message");
-        // console.log(div.getElementsByClassName("message"));
-        that.sendMessage(div.getElementsByClassName("message")[0].value); return false;
+        that.sendMessage();
+        return false;
     };
 
     div.appendChild(inputButton);
 
-    that.sendMessage = function (message) {
-        that.messages.push({message: message, time: new Date()});
+
+    that.init = function () {
+        if(that.messages[0] !== undefined) {
+            that.renderMessages();
+        }
+
     };
 
-    that.messages = [];
+    that.renderMessages = function () {
+        that.messages.forEach(function (message) {
+            that.renderMessage(message);
+        });
+    };
+
+    that.renderMessage = function (message) {
+        var messageArea = div.getElementsByClassName("messages-div")[0];
+        var p = document.createElement("p");
+
+        console.log(message);
+
+        p.appendChild(document.createTextNode(message.text));
+        messageArea.appendChild(p);
+    };
+
+
+    that.sendMessage = function () {
+        var textarea = div.getElementsByClassName("message")[0];
+        var message = {text: textarea.value, time: new Date()};
+
+        that.messages.push(message);
+        textarea.value = "";
+        that.renderMessage(message);
+    };
 }
 
 window.onload = function() {
     var MessageBoard1 = new MessageBoard("board1");
     var MessageBoard2 = new MessageBoard("board2");
+
+    MessageBoard1.init();
+    // MessageBoard1.renderMessage({text: "Hohohohooh", time: new Date()});
 };
