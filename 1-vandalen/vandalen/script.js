a0e18791fc2303d8bc209765fe04dcc7e09168fa
Robin Karlsson <rk222ev@github.com>
2014-11-03 16:32:14 +0100

    Fixes according to JSLint.

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,40 +1,41 @@
-"use strict";
+var makePerson = function (persArr) {
+    "use strict";
+    persArr = [{age: 15, name: "R"}];
 
-var makePerson = function(persArr){
+    if (!(Array.isArray(persArr)) && persArr[0] === undefined) {
+        throw new TypeError("The argument is empty or it is not an array.");
+    }
 
-	if(!(Array.isArray(persArr)) && persArr[0] === undefined) {
-		throw new TypeError("The argument is empty or it is not an array.");
-	}
+    var ages = persArr.map(
+        function (person) {
+            if (person.hasOwnProperty("age")) {
+                return person.age;
+            }
 
-	var ages = persArr.map(
-		function(person) {
-			if ("age" in person) {
-				return person.age;
-			} else if ("born" in person) {
-				return new Date().getFullYear() - new Date(person.born).getFullYear();
-			}
+            if (person.HasOwnProperty("born")) {
+                return new Date().getFullYear() - new Date(person.born).getFullYear();
+            }
+            throw new Error("The person does not contain the key age or the key born.");
+        }
+    ),
 
-			throw new Error("The person does not contain the key age or the key born.");
-		}
-	);
+        average = function (numbers) {
+            return numbers.reduce(function (a, b) { return a + b; }) / numbers.length;
+        },
 
- 	var average = function(numbers) {
-		return numbers.reduce(function(a, b) { return a + b;}) / numbers.length;
-	};
+        names = persArr.map(function (person) {
+            if (person.HasOwnProperty("name")) {
+                return person.name;
+            }
+            throw new Error("The person does not contain the key name.");
+        }).sort(function (a, b) {
+            return a.localeCompare(b);
+        }).join(", ");
 
-	var names = persArr.map(function(person) {
-		if ("name" in person) {
-			return person.name;
-		}
-		throw new Error("The person does not contain the key name.");
-	}).sort(function(a, b) {
-			return a.localeCompare(b);
-	}).join(", ");
-
-	return {
-		names: 		names,
-		minAge: 	Math.min.apply(null, ages),
-		maxAge: 	Math.max.apply(null, ages),
-		averageAge: Math.round(average(ages))
-	};
+    return {
+        names: names,
+        minAge: Math.min.apply(null, ages),
+        maxAge: Math.max.apply(null, ages),
+        averageAge: Math.round(average(ages))
+    };
 };
