32cb859874116b51c2ccabe3d10cae4f8556c0e4
Robin Karlsson <rk222ev@github.com>
2014-11-12 10:40:04 +0100

    Moved document functions to the run file.

--- 2-labbymezzage/js/messageBoard_run.js
+++ 2-labbymezzage/js/messageBoard_run.js
@@ -3,9 +3,53 @@ window.onload = function () {
 
   var messageBoard1 =  new MessageBoard("board1"),
     messageBoard2 = new MessageBoard("board2");
-
-//  messageBoard1.init();
-//  messageBoard2.init();
-
 };
+
+
+// Builds an DOM node based on a object with certain keys.
+document.buildElement = function (params) {
+
+  if (params.hasOwnProperty("element")) {
+    element = document.createElement(params.element);
+  }
+
+  if (params.hasOwnProperty("className")) {
+    element.className = params.className;
+  }
+
+  if (params.hasOwnProperty("innerHTML")) {
+    element.innerHTML = params.innerHTML;
+  }
+
+  if (params.hasOwnProperty("onclick")) {
+    element.onclick = params.onclick;
+  }
+
+  if (params.hasOwnProperty("src")) {
+    element.src = params.src;
+  }
+
+  if (params.hasOwnProperty("type")) {
+    element.type = params.type;
+  }
+
+  if (params.hasOwnProperty("value")) {
+    element.value = params.value;
+  }
+
+  return element;
+};
+
+
+// Gets DOM nodes based on id and class.
+document.getNode =  function (element, className) {
+  var boardNode =  document.getElementById(element);
+
+  if(className === undefined)  {
+    return boardNode;
+  }
+
+  return boardNode.getElementsByClassName(className)[0];
+};
+
 
