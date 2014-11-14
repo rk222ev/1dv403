17f5cff8dd09835e73c0dd56dd30d63942dd3e50
Robin Karlsson <rk222ev@github.com>
2014-11-14 09:02:43 +0100

    Created and linked files to the HTML document.

---
+++ 3-gameon/memory/js/Memory.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var Memory = function (rows, cols, node) {
+
+  this.getNode = function () { return document.getNode(node); };
+
+  this.getSize = function () { return { rows: rows, cols: cols }; };
+
+};
+
+Memory.prototype.start = function () {
+
+  var gameNode = this.getNode();
+
+  console.log("At start()");
+
+  var board = document.buildElement({ element: "div", className: "board", innerHTML: "Test"});
+
+  gameNode.appendChild(board);
+
+
+
+
+
+};
---
+++ 3-gameon/memory/js/memory_run.js
@@ -0,0 +1,65 @@
+"usr strict";
+var MemoryApp = {
+
+  init: function(){
+    var mem1 = new Memory(3,4,"game1");
+    // var mem2 = new Memory(3,4,"game2");
+    mem1.start();
+    // mem2.start();
+  }
+
+};
+
+window.onload = function () {
+
+  memoryApp = MemoryApp.init();
+
+};
+
+
+// Builds an DOM node based on a object with certain keys.
+document.buildElement = function (params) {
+
+  if (params.hasOwnProperty("element")) {
+    element = document.createElement(params.element);
+  }
+
+  if (params.hasOwnProperty("className")) {
+    element.className = params.className;
+  }
+
+  if (params.hasOwnProperty("innerHTML")) {
+    element.innerHTML = params.innerHTML;
+  }
+
+  if (params.hasOwnProperty("onclick")) {
+    element.onclick = params.onclick;
+  }
+
+  if (params.hasOwnProperty("src")) {
+    element.src = params.src;
+  }
+
+  if (params.hasOwnProperty("type")) {
+    element.type = params.type;
+  }
+
+  if (params.hasOwnProperty("value")) {
+    element.value = params.value;
+  }
+
+  return element;
+};
+
+
+// Gets DOM nodes based on id and class.
+document.getNode =  function (element, className) {
+  var boardNode =  document.getElementById(element);
+
+  if(className === undefined)  {
+    return boardNode;
+  }
+
+  return boardNode.getElementsByClassName(className)[0];
+};
+
