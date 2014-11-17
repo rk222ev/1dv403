68bdb3af1cbc9e69ddbf838a05e70950d239a75e
Robin Karlsson <rk222ev@student.lnu.se>
2014-11-17 11:30:30 +0000

    Checks if document.buildElements exists before overwriting it.

--- 2-labbymezzage/js/messageBoard_run.js
+++ 2-labbymezzage/js/messageBoard_run.js
@@ -6,39 +6,43 @@ window.onload = function () {
 };
 
 
-// Builds an DOM node based on a object with certain keys.
-document.buildElement = function (params) {
 
-  if (params.hasOwnProperty("element")) {
-    element = document.createElement(params.element);
-  }
+if (document.buildElement === undefined) {
+  // Builds an DOM node based on a object with certain keys.
+  document.buildElement = function (params) {
+  
+    if (params.hasOwnProperty("element")) {
+      element = document.createElement(params.element);
+    }
+  
+    if (params.hasOwnProperty("className")) {
+      element.className = params.className;
+    }
+  
+    if (params.hasOwnProperty("innerHTML")) {
+      element.innerHTML = params.innerHTML;
+    }
+  
+    if (params.hasOwnProperty("onclick")) {
+      element.onclick = params.onclick;
+    }
+  
+    if (params.hasOwnProperty("src")) {
+      element.src = params.src;
+    }
+  
+    if (params.hasOwnProperty("type")) {
+      element.type = params.type;
+    }
+  
+    if (params.hasOwnProperty("value")) {
+      element.value = params.value;
+    }
+  
+    return element;
+  };
+}
 
-  if (params.hasOwnProperty("className")) {
-    element.className = params.className;
-  }
-
-  if (params.hasOwnProperty("innerHTML")) {
-    element.innerHTML = params.innerHTML;
-  }
-
-  if (params.hasOwnProperty("onclick")) {
-    element.onclick = params.onclick;
-  }
-
-  if (params.hasOwnProperty("src")) {
-    element.src = params.src;
-  }
-
-  if (params.hasOwnProperty("type")) {
-    element.type = params.type;
-  }
-
-  if (params.hasOwnProperty("value")) {
-    element.value = params.value;
-  }
-
-  return element;
-};
 
 
 // Gets DOM nodes based on id and class.
