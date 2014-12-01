c131cd8e8537b8c56362bb30bfc9c701582ae4b8
Robin Karlsson <rk222ev@github.com>
2014-12-01 13:39:10 +0100

    Added a skeleton app for the Rss reader app.

---
+++ 5-pwd/js/apps/rssReader/RssReader.js
@@ -0,0 +1,20 @@
+PWD.apps.RssReader = function (params) {
+
+  var id              = params.id,
+      appSettings     = {},
+      windowSettings  = {},
+      picData;
+
+
+  windowSettings.id           = id;
+  windowSettings.icon         = "pics/icons/RssReader.svg";
+  windowSettings.titleBarText = "RssReader 0.1";
+  windowSettings.width        = params.width || 400;
+  windowSettings.height       = params.height || 600;
+
+
+  this.window = new PWD.Window(windowSettings);
+  this.node   = this.window.node.querySelector(".app");
+
+
+};
