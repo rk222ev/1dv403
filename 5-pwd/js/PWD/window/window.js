2374b9f3845f5ee4691dda6a56c0cf24083d350a
Robin Karlsson <rk222ev@github.com>
2015-01-09 07:56:35 +0100

    Cleanup.

--- 5-pwd/js/PWD/window/window.js
+++ 5-pwd/js/PWD/window/window.js
@@ -1,85 +1,88 @@
 "use strict"
+
+/* global define, document, $ */
+
 define(["pwd/helper/settings"], function (settings) {
 
-  var Window = function (id) {
+    var Window = function (id) {
 
-    this.width = 500;
-    this.height = 400;
-    this.top = 10;
-    this.left = 10;
-    this.caughtX = 0;
-    this.caughtY = 0;
-    this.maximized = false;
+        this.width = 500;
+        this.height = 400;
+        this.top = 10;
+        this.left = 10;
+        this.caughtX = 0;
+        this.caughtY = 0;
+        this.maximized = false;
 
-    this.icons = {
-        close:    "pics/icons/clear.svg",
-        maximize: "pics/icons/chevron-up.svg",
-        minimize: "pics/icons/chevron-down.svg",
-        loading:  "pics/ajax-loader.gif",
-        placeholder: "pics/ajax-placeholder.gif",
-        settings: "pics/icons/cog.svg",
+        this.icons = {
+            close:    "pics/icons/clear.svg",
+            maximize: "pics/icons/chevron-up.svg",
+            minimize: "pics/icons/chevron-down.svg",
+            loading:  "pics/ajax-loader.gif",
+            placeholder: "pics/ajax-placeholder.gif",
+            settings: "pics/icons/cog.svg",
+        };
+
+        this.getId = function () { return id; };
+
     };
-
-    this.getId = function () { return id; };
-
-  };
 
 
   // Sets the text displayed on the window statusbar.
-  Window.prototype.setStatusbarText = function (text) {
-    $('#' + this.getId()).find(".statusbar-text").text(text);
+    Window.prototype.setStatusbarText = function (text) {
+        $('#' + this.getId()).find(".statusbar-text").text(text);
 
-  };
+    };
 
 
   //Updates the windows position.
-  Window.prototype.setPosition = function (x, y) {
-    var node = $('#' + this.getId());
-    var maxX = settings.width - this.width;
-    var maxY = settings.height - this.height;
+    Window.prototype.setPosition = function (x, y) {
+        var node = $('#' + this.getId());
+        var maxX = settings.width - this.width;
+        var maxY = settings.height - this.height;
 
-    /*
-        Checks window posistions to make sure it isnt placed
-        outside of the "desktop".
-    */
-    if (x || y) {
+        /*
+            Checks window posistions to make sure it isnt placed
+            outside of the "desktop".
+        */
+        if (x || y) {
 
-        this.left += x;
-        if (this.left < 0) {
-            this.left = 0;
-        } else if (this.left > maxX) {
-            this.left = maxX;
+            this.left += x;
+            if (this.left < 0) {
+                this.left = 0;
+            } else if (this.left > maxX) {
+                this.left = maxX;
+            }
+
+            this.top += y;
+            if (this.top < 0) {
+                this.top = 0;
+            } else if (this.top > maxY) {
+                this.top = maxY;
+            }
         }
 
-        this.top += y;
-        if (this.top < 0) {
-            this.top = 0;
-        } else if (this.top > maxY) {
-            this.top = maxY;
+        node.css('left', this.left + 'px');
+        node.css('top', this.top + 'px');
+    };
+
+
+    Window.prototype.setOpeningPosition = function () {
+        var openedWindows = document.querySelectorAll(".pwd .app").length;
+        var offset = openedWindows * 10;
+        var x = this.left + offset % (settings.width - this.width);
+        var y = this.top + offset % (settings.height - this.height);
+        var sizeDiff = settings.height - this.height - this.top;
+
+        // Make sure too large windows are not extending outside the desktop.
+        if (sizeDiff < 0) {
+            x = 0;
+            this.height = sizeDiff + settings.height;
+            document.getElementById(this.getId()).style.height = this.height + "px";
         }
-    }
 
-    node.css('left', this.left + 'px');
-    node.css('top', this.top + 'px');
-  };
-
-
-  Window.prototype.setOpeningPosition = function () {
-    var openedWindows = document.querySelectorAll(".pwd .app").length;
-    var offset = openedWindows * 10;
-    var x = this.left + offset % (settings.width - this.width);
-    var y = this.top + offset % (settings.height - this.height);
-    var sizeDiff = settings.height - this.height - this.top;
-
-    // Make sure too large windows are not extending outside the desktop.
-    if (sizeDiff < 0) {
-        x = 0;
-        this.height = sizeDiff + settings.height;
-        document.getElementById(this.getId()).style.height = this.height + "px";
-    }
-
-    this.setPosition(x, y);
-  };
+        this.setPosition(x, y);
+    };
 
 
     Window.prototype.setAsLoaded = function () {
